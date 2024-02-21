39fc610c74129e0e52c87abdb7029ee36923be9a
Uhyeon Lim <djdngus32@naver.com>
2024-02-22 02:20:36 +0900

    게임 데이터 매니저 추가
    게임 데이터 저장, 불러오기 기능 담당

---
+++ Assets/1_Scripts/Manager/GameDataManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #region ╥ндц
+
+    public void SaveDataToLocal(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    public void SaveDataToLocal(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+    }
+
+    public int LoadDataToLocal(string key, int defaultValue = 0)
+    {
+        return PlayerPrefs.GetInt(key, defaultValue);
+    }
+
+    public float LoadDataToLocal(string key, float defaultValue = 0f)
+    {
+        return PlayerPrefs.GetFloat(key, defaultValue);
+    }
+
+    #endregion
+}
