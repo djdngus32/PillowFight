4641b14a584b7b31c59d15c4ec24569c432d021b
Uhyeon Lim <djdngus32@naver.com>
2024-02-26 01:02:49 +0900

    데미지 트리거 로컬로만 작동하게 변경

--- Assets/1_Scripts/DamageTrigger.cs
+++ Assets/1_Scripts/DamageTrigger.cs
@@ -3,53 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
+/// <summary>
+/// 로컬로만 동작하게 할 데미지 트리거
+/// </summary>
 public class DamageTrigger : MonoBehaviour
 {
     [SerializeField] private int damage;
-
-    private PlayerRef ownerPlayer = PlayerRef.None;
-    private Collider triggerCollider;
-
-    public bool IsEnable => triggerCollider != null && triggerCollider.enabled;
-
-    private void Awake()
-    {
-        triggerCollider = GetComponent<Collider>();
-    }
-
-    private void Start()
-    {
-        if(triggerCollider != null && triggerCollider.isTrigger == false)
-        {
-            triggerCollider.isTrigger = true;
-        }
-    }
-
-    public void EnableDamageTrigger(PlayerRef ownerPlayer, int damage)
-    {
-        if(triggerCollider != null)
-        {
-            this.damage = damage;
-            this.ownerPlayer = ownerPlayer;
-            triggerCollider.enabled = true;
-        }
-    }
-
-    public void DisableDamageTrigger()
-    {
-        if (triggerCollider != null)
-        {
-            triggerCollider.enabled = false;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent<PlayerStat>(out var playerStat))
         {
-            if(ownerPlayer != playerStat.Object.StateAuthority)
+            if(playerStat.Object.HasStateAuthority)
             {
-                playerStat.RPC_ApplyDamage(ownerPlayer, damage);
+                playerStat.RPC_ApplyDamage(PlayerRef.None, damage);
             }            
         }
     }
